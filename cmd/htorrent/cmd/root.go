957984bbb7bd1ddc59baf17a8787896f0cbb43d7
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-06-17 20:23:58 +0200

    feat: Add proper command structure

---
+++ cmd/htorrent/cmd/root.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"strings"
+
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	verboseFlag = "verbose"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "htorrent",
+	Short: "HTTP to BitTorrent gateway with seeking support",
+	Long: `Access torrents just like you would access any file served over a webserver!
+
+
+Find more information at:
+https://github.com/pojntfx/htorrent`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		viper.SetEnvPrefix("htorrent")
+		viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_", ".", "_"))
+
+		if err := viper.BindPFlags(cmd.PersistentFlags()); err != nil {
+			return err
+		}
+
+		switch viper.GetInt(verboseFlag) {
+		case 0:
+			zerolog.SetGlobalLevel(zerolog.Disabled)
+		case 1:
+			zerolog.SetGlobalLevel(zerolog.PanicLevel)
+		case 2:
+			zerolog.SetGlobalLevel(zerolog.FatalLevel)
+		case 3:
+			zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+		case 4:
+			zerolog.SetGlobalLevel(zerolog.WarnLevel)
+		case 5:
+			zerolog.SetGlobalLevel(zerolog.InfoLevel)
+		case 6:
+			zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		default:
+			zerolog.SetGlobalLevel(zerolog.TraceLevel)
+		}
+
+		return nil
+	},
+}
+
+func Execute() error {
+	rootCmd.PersistentFlags().IntP(verboseFlag, "v", 5, "Verbosity level (0 is disabled, default is info, 7 is trace)")
+
+	if err := viper.BindPFlags(rootCmd.PersistentFlags()); err != nil {
+		return err
+	}
+
+	viper.AutomaticEnv()
+
+	return rootCmd.Execute()
+}
